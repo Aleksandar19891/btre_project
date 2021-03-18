fd49900d2dff438f297f835299d63c01200c1b83
Default <acozeljkovic@gmail.com>
2021-03-18 22:37:28 +0100

    Adding local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -150,3 +150,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'acozeljkovic1989@gmail.com'
 EMAIL_HOST_PASSWORD = 'delije sever1989'
 EMAIL_USE_TLS = True
+
+try:
+    from local_settigs import *
+except ImportError:
+    pass
